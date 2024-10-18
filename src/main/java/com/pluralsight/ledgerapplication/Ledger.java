13b62b44a5ebe196dd00980a95c3eee849edd5e8
Keely Lowder <keelybowman@icloud.com>
2024-10-18 09:12:19 -0400

    trying to fix errors

--- src/main/java/com/pluralsight/ledgerapplication/Ledger.java
+++ src/main/java/com/pluralsight/ledgerapplication/Ledger.java
@@ -33,63 +33,63 @@ public class Ledger {
 
     // // reads from csv file and displays all the payments
     public static void allPayments(ArrayList<Transaction> allPayment) {
-        for (Transaction p : allPayment) ;
-        if (p.getAmount() < 0) {
-            System.out.println(p);
+        for (Transaction p : allPayment) {
+            {
+                if (p.getAmount() < 0) {
+                    System.out.println(p);
+                }
+            }
         }
     }
-
-
-    public static void allReports(ArrayList<Transaction> allReports) {
-        for (Transaction r : allReports) ;
-        System.out.println();
-    }
-
-
-    // reports
-    public static void monthToDate(ArrayList<Transaction> monthToDate) {
-        for (Transaction mtd : transactions) ;
-        if (Transaction.getDate().getMonth() == (LocalDate.now().getMonth())) {
-            System.out.print("Month to date:" + transactions);
-
-        }
-    }
-
-    public static void previousMonth(ArrayList<Transaction> previousMonth) {
-        for (Transaction pm : transactions)
-            if (Transaction.getDate().getMonth() - 1 == LocalDate.now().getMonth()) {
-                System.out.println("Previous month:" + transactions);
-
+            // // reads from csv file and displays all the reports
+            public static void allReports (ArrayList < Transaction > allReports) {
+                for (Transaction r : allReports) ;
+                System.out.println();
             }
 
 
-    }
+            // methods for reports
+            public static void monthToDate (ArrayList < Transaction > monthToDate) {
+                for (Transaction mtd : transactions) ;
+                if (Transaction.getDate().getMonth() == (LocalDate.now().getMonth())) {
+                    System.out.print("Month to date:" + transactions);
 
-    public static void yearToDate(ArrayList<Transaction> yearToDate) {
-        for (Transaction ytd : yearToDate) ;
-        if (Transaction.date.getYear() == LocalDate.now().getYear()) ;
-        System.out.println("Year to Date: " + yearToDate);
-    }
+                }
+            }
+            // filters to display the transactions from the previous month
+            public static void previousMonth (ArrayList < Transaction > previousMonth) {
+                for (Transaction pm : transactions) {
+                    if ((Transaction.date.getMonth() - 1) == LocalDate.now().getMonth()) {
+                        System.out.println("Previous month:" + transactions);
 
-
-    public static void previousYear(ArrayList<Transaction> previousYear) {
-        for (Transaction py : previousYear) ;
-        if ((Transaction.date.getYear() - 1) == LocalDate.now().getYear()) ;
-        System.out.println("Previous year: " + previousYear);
-    }
-
-
-    public static void searchVendor(ArrayList<Transaction> searchVendor) {
-        for (Transaction sv : searchVendor) {
-            if (Transaction.vendor.getVendor().equalsIgnoreCase(searchVendor)) ;
-            System.out.println("Please enter name of vendor" + searchVendor);
-        }
-        {
+                    }
 
 
-        }
+                }
+            }
+            public static void yearToDate (ArrayList < Transaction > yearToDate) {
+                for (Transaction ytd : yearToDate) ;
+                if (Transaction.date.getYear() == LocalDate.now().getYear()) ;
+                System.out.println("Year to Date: " + yearToDate);
+            }
+
+            // filters to display the transactions from the previous year
+            public static void previousYear (ArrayList < Transaction > previousYear) {
+                for (Transaction py : previousYear) {
+                    if ((Transaction.date.getYear() - 1) == LocalDate.now().getYear()) ;
+                    System.out.println("Previous year: " + previousYear);
+                }
+            }
+
+            public static void searchVendor (ArrayList < Transaction > searchVendor) {
+                for (Transaction sv : searchVendor) {
+                    if (Transaction.vendor.getVendor().equalsIgnoreCase(searchVendor)) ;
+                    System.out.println("Please enter name of vendor" + searchVendor);
+                }
 
 
-    }
-}
 
+                }
+
+
+            }
