9d2650ae6b65d57351471381e418c4b37df653ce
Keely Lowder <keelybowman@icloud.com>
2024-10-31 16:06:01 -0400

    fixed one error but still cant figure out the previous month method. i think everything runs right except for my reports screen

--- src/main/java/com/pluralsight/ledgerapplication/MenuApp.java
+++ src/main/java/com/pluralsight/ledgerapplication/MenuApp.java
@@ -243,7 +243,7 @@ public class MenuApp {
 
     public static void showAllReports() {
         System.out.println("All reports: ");
-        Ledger.allReports(transactions);
+        reportsDisplay();
 
 
     }
